9d4341b267e0e91363b7dfdf17ff4379a040fbf8
Gabriel D'Espindula <gabrieldespindula@gmail.com>
2019-07-02 09:29:35 +0200

    Update heartRateSensor.js

--- webpage/heartRateSensor.js
+++ webpage/heartRateSensor.js
@@ -8,7 +8,7 @@
       this._characteristics = new Map();
     }
     connect() {
-      return navigator.bluetooth.requestDevice({filters:[{services:[ 'heart_rate' ]}]})
+      return navigator.bluetooth.requestDevice({filters:[{services:[ 'TKBA 71' ]}]})
       .then(device => {
         this.device = device;
         return device.gatt.connect();
