acd3f60c58e1d2c8769f94163ca6ac4a2afb5852
Gabriel D'Espindula <gabrieldespindula@gmail.com>
2019-08-15 17:16:25 +0200

    Update app.js

--- webpage/app.js
+++ webpage/app.js
@@ -1,5 +1,19 @@
-var canvas = document.querySelector('canvas');
+var canvas = document.querySelector('#waves');
+var ccdCanvas = document.querySelector('#ccdDisplay');
 var statusText = document.querySelector('#statusText');
+var lbIntTime = document.getElementById("intTime");
+var lbLaserDist = document.getElementById("laserDistance");
+var lbLaserStart = document.getElementById("laserDetectPx");
+var lbLaserLength = document.getElementById("laserWaveLength");
+var ccdMode = document.getElementById("ccdModeSelector");
+
+var heartRates = [];
+var accelerometerX = [];
+var accelerometerY = [];
+var accelerometerZ = [];
+var laserStart, laserLenght;
+var chartLenght = 256;
+var ccd = Array(260).fill(0);
 
 statusText.addEventListener('click', function() {
   statusText.textContent = 'Connecting...';
@@ -7,23 +21,23 @@ statusText.addEventListener('click', function() {
   accelerometerX = [];
   accelerometerY = [];
   accelerometerZ = [];
-  heartRateSensor.connect()
-  .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement))
-  .then(() => heartRateSensor.startNotificationsCCD().then(handleCCD))
+  tkba.connect()
+  .then(() => tkba.startNotificationsAccelerometer().then(handleAccelerometer))
+  .then(() => tkba.startNotificationsCCD().then(handleCCD))
   .catch(error => {
     statusText.textContent = error;
   });
 });
 
-function handleHeartRateMeasurement(heartRateMeasurement) {
+function handleAccelerometer(accData) {
   console.log('Accelerometer event settled');
   var postscale = 0;
   
-  heartRateMeasurement.addEventListener('characteristicvaluechanged', event => {
-    console.log('New notification - ' + event.target.value.getUint8(0) + ' ' + event.target.value.getUint8(1) + ' ' + event.target.value.getUint8(2));
+  accData.addEventListener('characteristicvaluechanged', event => {
+    //console.log('New notification - ' + event.target.value.getUint8(0) + ' ' + event.target.value.getUint8(1) + ' ' + event.target.value.getUint8(2));
     //statusText.textContent = event.target.value.getUint8(0) + event.target.value.getUint8(1) + event.target.value.getUint8(2) + event.target.value.getUint8(3) + event.target.value.getUint8(1) + event.target.value.getUint8(4) + event.target.value.getUint8(5);
     var accX, accY, accZ;
-    //var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
+    
     var sign = event.target.value.getUint8(1) & (1 << 7);
     var accX = (((event.target.value.getUint8(1) & 0xFF) << 8) | (event.target.value.getUint8(0) & 0xFF));
     if (sign) {
@@ -45,135 +59,184 @@ function handleHeartRateMeasurement(heartRateMeasurement) {
       postscale=0;
 //      heartRates.push(accY);
 //     accelerometerX.push(accX);
-      accelerometerY.push(accY);
+//      accelerometerY.push(accY);
 //      accelerometerZ.push(accZ);
-      drawWaves();
+//      drawWaves();
     }
   });
 }
 
-function handleCCD(heartRateMeasurement) {
+function handleCCD(ccdData) {
   console.log('CCD event settled');
   var postscale = 0;
   
-  heartRateMeasurement.addEventListener('characteristicvaluechanged', event => {
-    //console.log('New notification - ' + event.target.value.getUint8(0) + ' ' + event.target.value.getUint8(1) + ' ' + event.target.value.getUint8(2));
-    //statusText.textContent = event.target.value.getUint8(0) + event.target.value.getUint8(1) + event.target.value.getUint8(2) + event.target.value.getUint8(3) + event.target.value.getUint8(1) + event.target.value.getUint8(4) + event.target.value.getUint8(5);
+  ccdData.addEventListener('characteristicvaluechanged', event => {
     statusText.textContent = event.target.byteLenght;
     //console.log(event.target.value);
-    if((event.target.value.getUint8(0) == 250 && event.target.value.getUint8(1) == 250) || postscale > 25){
-      postscale=0;
+    
+    if(ccdMode.innerHTML === "3"){
+  
+      var data = [event.target.value.getUint8(5),
+                  event.target.value.getUint8(4),
+                  event.target.value.getUint8(3),
+                  event.target.value.getUint8(2)];
+      var buf = new ArrayBuffer(4);
+      var view = new DataView(buf);
+      data.forEach(function (b, i) {
+          view.setUint8(i, b);
+      });
+      var num = view.getFloat32(0);
+      lbLaserDist.innerHTML = num.toString() + "mm";
+      lbIntTime.innerHTML = (event.target.value.getUint8(0)<<8 | event.target.value.getUint8(1)).toString() + " CPU clocks";
+
+      ccd.fill(0);
+      ccd[Math.round(num/0.0635)] = 2800;
+      
+
+      /*
+      console.log(event.target.value.getUint8(0).toString() + " " +
+                  event.target.value.getUint8(1).toString() + " " +
+                  event.target.value.getUint8(2).toString() + " " +
+                  event.target.value.getUint8(3).toString() + " " +
+                  event.target.value.getUint8(4).toString() + " " +
+                  event.target.value.getUint8(5).toString() );
+      */
     }
-    for(var i=0 ; i<20 ; i+=2){
-      ccd[postscale*10+i/2]=(((event.target.value.getUint8(i) & 0xFF) << 8) | (event.target.value.getUint8(i+1) & 0xFF));
+    else {
+
+      if((event.target.value.getUint8(0) == 0xac && event.target.value.getUint8(5) == 0xac) || postscale > 25){
+        postscale=0;
+
+        laserStart = event.target.value.getUint8(3);
+        laserLenght = event.target.value.getUint8(4);
+
+        if(ccdMode.innerHTML === "2"){
+          chartLenght = laserLenght;
+        }
+        
+        lbLaserStart.innerHTML = "pixel number " + laserStart.toString();
+        lbLaserLength.innerHTML = laserLenght.toString() + " pixels";
+        lbIntTime.innerHTML = (event.target.value.getUint8(1)<<8 | event.target.value.getUint8(2)).toString() + " CPU clocks";
+        lbLaserDist.innerHTML = ((event.target.value.getUint8(3)+(event.target.value.getUint8(4).toString()/2))*0.0635).toString() + "mm";
+      }
+
+      for(var i=0 ; i<20 ; i+=2){
+        ccd[postscale*10+i/2]=(((event.target.value.getUint8(i) & 0xFF) << 8) | (event.target.value.getUint8(i+1) & 0xFF));
+      }
+      if(ccdMode.innerHTML === "1"){
+        ccd[postscale*10+10]=0xaaaaaaaa; //Add a peak just to visualize the "scanning"
+      }
+
+      postscale++;
     }
-    ccd[postscale*10+10]=0xaaaaaaaa;
-    postscale++;
+
+    drawCCD();
     drawWaves();
   });
 }
 
-var heartRates = [];
-var accelerometerX = [];
-var accelerometerY = [];
-var accelerometerZ = [];
-var ccd = Array(260).fill(0);
-var mode = 'line';
+function drawCCD() {
+  requestAnimationFrame(() => {
+    ccdCanvas.width = parseInt(getComputedStyle(canvas).width);
+    ccdCanvas.height = 50;
+    
+    var context = ccdCanvas.getContext('2d');
+    context.clearRect(0, 0, ccdCanvas.width, ccdCanvas.height);
 
-//canvas.addEventListener('click', event => {
-//  mode = mode === 'bar' ? 'line' : 'bar';
-//  drawWaves();
-//});
+    var square_side = canvas.width/chartLenght;
+    for(var i = 3; i<258 ; i++)
+    {
+      var pixel = ((ccd[i]>>4).toString(16));
+      var RR = ((pixel.length==1)?("0"+pixel):(pixel));
+      context.fillStyle = '#'+RR+'0000';
+      context.fillRect((i-3)*square_side, 0, square_side, 50);
+      context.stroke();
+    }
+  });
+}
+
 
 function drawWaves() {
   requestAnimationFrame(() => {
     canvas.width = parseInt(getComputedStyle(canvas).width.slice(0, -2)) * devicePixelRatio;
-    canvas.height = parseInt(getComputedStyle(canvas).height.slice(0, -2)) * devicePixelRatio;
+    canvas.height = 200;//parseInt(getComputedStyle(canvas).height.slice(0, -2)) * devicePixelRatio;
 
     var context = canvas.getContext('2d');
     var margin = 2;
     var max = Math.max(0, Math.round(canvas.width / 11));
     var offset = Math.max(0, heartRates.length - max);
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.strokeStyle = '#581845';
-    if (mode === 'bar') {
-      for (var i = 0; i < Math.max(heartRates.length, max); i++) {
-        var barHeight = Math.round(5*canvas.height/6 + heartRates[i + offset ]/100);//Math.round(heartRates[i + offset ] * canvas.height / 200);
-        context.rect(11 * i + margin, canvas.height - barHeight, margin, Math.max(0, barHeight - margin));
-        context.stroke();
-      }
-    } else if (mode === 'line') {
-      context.beginPath();
-      context.lineWidth = 6;
-      context.lineJoin = 'round';
-      context.shadowBlur = '1';
-      context.shadowOffsetY = '1';
-      for (var i = 0; i < Math.max(accelerometerX.length, max); i++) {
-        var lineHeight = Math.round(5*canvas.height/6 + accelerometerX[i + offset ]/100);//Math.round(heartRates[i + offset ] * canvas.height / 200);
-        if (i === 0) {
-          context.moveTo(11 * i, canvas.height - lineHeight);
-        } else {
-          context.lineTo(11 * i, canvas.height - lineHeight);
-        }
-        context.stroke();
-      }
-      context.beginPath();
-      context.lineWidth = 6;
-      context.lineJoin = 'round';
-      context.shadowBlur = '1';
-      context.strokeStyle = '#900c3f';
-      context.shadowOffsetY = '1';
-      for (var i = 0; i < Math.max(accelerometerY.length, max); i++) {
-        var lineHeight = Math.round(3*canvas.height/6 + accelerometerY[i + offset ]/100);//Math.round(heartRates[i + offset ] * canvas.height / 200);
-        if (i === 0) {
-          context.moveTo(11 * i, canvas.height - lineHeight);
-        } else {
-          context.lineTo(11 * i, canvas.height - lineHeight);
-        }
-        context.stroke();
-      }
-      context.beginPath();
-      context.lineWidth = 6;
-      context.lineJoin = 'round';
-      context.shadowBlur = '1';
-      context.strokeStyle = 'c70039';
-      context.shadowOffsetY = '1';
-      for (var i = 0; i < Math.max(accelerometerZ.length, max); i++) {
-        var lineHeight = Math.round(1*canvas.height/6 + accelerometerZ[i + offset ]/100);//Math.round(heartRates[i + offset ] * canvas.height / 200);
-        if (i === 0) {
-          context.moveTo(11 * i, canvas.height - lineHeight);
-        } else {
-          context.lineTo(11 * i, canvas.height - lineHeight);
-        }
-        context.stroke();
-      }
-    }
-    
-    var square_side = canvas.width/256;
-    for(var i = 0; i<256 ; i++)
-    {
-      var pixel = ((ccd[i]>>4).toString(16)&0xff);
-      var RR = ((pixel.length==1)?("0"+pixel):(pixel));
-      context.fillStyle = '#'+RR+'0000';
-      context.fillRect(i*square_side, 30, square_side, 30);
-      context.stroke();
 
-      context.lineWidth = 6;
-      context.lineJoin = 'round';
-      context.shadowBlur = '1';
+    context.beginPath();
+    context.lineWidth = 6;
+    context.lineJoin = 'round';
+    context.shadowBlur = '1';
+    context.strokeStyle = '#581845';
+    context.shadowOffsetY = '1';
+    for (var i = 0; i < Math.max(accelerometerX.length, max); i++) {
+      var lineHeight = Math.round(5*canvas.height/6 + accelerometerX[i + offset ]/100);//Math.round(heartRates[i + offset ] * canvas.height / 200);
       if (i === 0) {
-        context.moveTo(square_side * i, -(ccd[i]>>4)&0xff);
+        context.moveTo(11 * i, canvas.height - lineHeight);
       } else {
-          context.lineTo(square_side * i, -(ccd[i]>>4)&0xff);
+        context.lineTo(11 * i, canvas.height - lineHeight);
+      }
+      context.stroke();
+    }
+
+    context.beginPath();
+    context.lineWidth = 6;
+    context.lineJoin = 'round';
+    context.shadowBlur = '1';
+    context.strokeStyle = '#900c3f';
+    context.shadowOffsetY = '1';
+    for (var i = 0; i < Math.max(accelerometerY.length, max); i++) {
+      var lineHeight = Math.round(3*canvas.height/6 + accelerometerY[i + offset ]/100);//Math.round(heartRates[i + offset ] * canvas.height / 200);
+      if (i === 0) {
+        context.moveTo(11 * i, canvas.height - lineHeight);
+      } else {
+        context.lineTo(11 * i, canvas.height - lineHeight);
+      }
+      context.stroke();
+    }
+
+    context.beginPath();
+    context.lineWidth = 6;
+    context.lineJoin = 'round';
+    context.shadowBlur = '1';
+    context.strokeStyle = 'c70039';
+    context.shadowOffsetY = '1';
+    for (var i = 0; i < Math.max(accelerometerZ.length, max); i++) {
+      var lineHeight = Math.round(1*canvas.height/6 + accelerometerZ[i + offset ]/100);//Math.round(heartRates[i + offset ] * canvas.height / 200);
+      if (i === 0) {
+        context.moveTo(11 * i, canvas.height - lineHeight);
+      } else {
+        context.lineTo(11 * i, canvas.height - lineHeight);
+      }
+      context.stroke();
+    }
+
+    context.beginPath();
+    context.lineWidth = 6;
+    context.lineJoin = 'round';
+    context.shadowBlur = '1';
+    
+    var square_side = canvas.width/chartLenght;
+    for(var i = 3; i<258 ; i++)
+    {
+      if (i === 3) {
+        context.moveTo(square_side * (i-3), canvas.height-((ccd[i]>>4)&0xff)-10);
+      } else {
+          context.lineTo(square_side * (i-3), canvas.height-((ccd[i]>>4)&0xff)-10);
       }
     }
+    context.stroke();
   });
 }
 
-window.onresize = drawWaves;
-
 document.addEventListener("visibilitychange", () => {
   if (!document.hidden) {
+    drawCCD();
     drawWaves();
   }
 });
+
